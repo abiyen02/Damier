efbfd8e734a3581e741955a1d6447a499eb87a59
Biyen Abdoul Hassib <biyen_hassib@yahoo.fr>
2021-01-04 18:29:38 +0100

    modificatio de pions

--- dame.py
+++ dame.py
@@ -30,7 +30,33 @@ def damier():
                     can1.create_rectangle(j*case, i*case, (j*case)+case, (i*case)+case, fill='black')
             j+=1
         i+=1
+        
+def pions ():
+	i=0
+	j=0
+	x= random.randint(50,400) 
+	y = random.randint(50,400) 
+	while i < 10:
+		while j < 10:
+			if (i%2)==0:
+				if (j%2)==0:
+					can1.create_oval(x,y,x+20,y+20,fill=(random.sample(COLORS, 1)[0]), 
+					outline = random.sample(COLORS, 1)[0],width=0)
+				else :
+					can1.create_oval(x,y,x+20,y+20,fill=(random.sample(COLORS, 1)[0]), 
+					outline = random.sample(COLORS, 1)[0],width=0)
+			else:
+				if (j%2)==0:
+					can1.create_oval(x,y,x+20,y+20,fill=(random.sample(COLORS, 1)[0]), 
+					outline = random.sample(COLORS, 1)[0],width=0)
+				else :
+					can1.create_oval(x,y,x+20,y+20,fill=(random.sample(COLORS, 1)[0]), 
+					outline = random.sample(COLORS, 1)[0],width=0)
+			j=j+1
+		i=i+1
 
+button = Button(root, text="jouer", command= pions)
+button.pack()
 
 b1 = Button(root, text ="Nouvelle partie ", command =damier)
 b1.pack()
